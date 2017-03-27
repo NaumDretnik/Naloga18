c8901bcd3bb8fcacaec51789323f44f786a79127
Naum <naumaram@gmail.com>
2017-03-27 14:50:50 +0200

    Še brez handlerja za izbrisana sporocila

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import jinja2
 import webapp2
-
+import cgi
 from models import Message
 
 
@@ -33,30 +33,90 @@ class MainHandler(BaseHandler):
     def get(self):
         return self.render_template("index.html")
 
+
 class GuestbookHandler(BaseHandler):
     def get(self):
         return self.render_template("guestbook.html")
 
+
 class MessageHandler(BaseHandler):
     def post(self):
-        msg = self.request.get("content")
-        name = self.request.get("name")
-        email = self.request.get("email")
+        msg = cgi.escape(self.request.get("content"))
+        name = cgi.escape(self.request.get("name"))
+        email = cgi.escape(self.request.get("email"))
         msg_save = Message(content=msg, name=name, email=email)
         msg_save.put()
         return self.render_template("sent.html")
 
+
 class MessagesHandler(BaseHandler):
     def get(self):
-        messages = Message.query().fetch()
+        messages = Message.query(Message.deleted == False).fetch()
         params = {
             "messages": messages
         }
         return self.render_template("messages.html", params)
+
+
+class MessageSingleHandler(BaseHandler):
+    def get(self, message_id):
+        message = Message.get_by_id(int(message_id))
+        params = {
+            "message": message
+        }
+        return self.render_template("message_single.html", params)
+
+
+class MessageEditHandler(BaseHandler):
+    def get(self, message_id):
+        message = Message.get_by_id(int(message_id))
+        params = {
+            "message": message
+        }
+        return self.render_template("message_edit.html", params)
+
+    def post(self, message_id):
+        message = Message.get_by_id(int(message_id))
+        message.name = cgi.escape(self.request.get("name"))
+        message.email = cgi.escape(self.request.get("email"))
+        message.content = cgi.escape(self.request.get("content"))
+        message.put()
+        return self.redirect("/messages")
+
+
+class MessageDeleteHandler(BaseHandler):
+    def get(self, message_id):
+        message = Message.get_by_id(int(message_id))
+        params = {
+            "message": message
+        }
+        return self.render_template("delete_message.html", params)
+
+    def post(self, message_id):
+        message = Message.get_by_id(int(message_id))
+        message.deleted = True
+        message.put()
+        return self.render_template("message_deleted.html")
+
+
+class DeletedMessagesHandler(BaseHandler):
+    def get(self):
+        messages = Message.query(Message.deleted == True).fetch()
+        params = {
+            "messages": messages
+        }
+        return self.render_template("deletedmessages.html", params)
+
+
+
 
 app = webapp2.WSGIApplication([
         webapp2.Route('/', MainHandler),
         webapp2.Route('/guestbook', GuestbookHandler),
         webapp2.Route('/message-sent', MessageHandler),
         webapp2.Route('/messages', MessagesHandler),
+        webapp2.Route('/<message_id:\d+>', MessageSingleHandler),
+        webapp2.Route('/message-edit-<message_id:\d+>', MessageEditHandler),
+        webapp2.Route('/message-delete-<message_id:\d+>', MessageDeleteHandler),
+        webapp2.Route('/deleted-messages', DeletedMessagesHandler),
 ], debug=True)
--- models.py
+++ models.py
@@ -2,8 +2,10 @@
 
 from google.appengine.ext import ndb
 
+
 class Message(ndb.Model):
     content = ndb.StringProperty()
     name = ndb.StringProperty()
     email = ndb.StringProperty()
     timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    deleted = ndb.BooleanProperty(default=False)
